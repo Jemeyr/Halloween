f94171fa92ae2a732fcf39d64c6d2198a0a7e5af
JM <jeremymohr1990@Hotmail.com>
2012-10-20 13:06:30 -0700

    Temp superzombie has some control stuff

--- Halloween/Halloween/Entities/SuperZombie.cs
+++ Halloween/Halloween/Entities/SuperZombie.cs
@@ -8,10 +8,141 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Halloween.Graphics;
+
 
 namespace Halloween.Entities
 {
+    enum PlayerState { Run, Jump, Lunge, Sit, LungeStunned }
+    //you can run with speed zero and that's still running. 
+
     class SuperZombie
     {
+        //inherited
+        public Vector2 pos;
+        public Animation animation;
+        public Vector2 vel;
+
+
+        GameTime actionStart;
+        public PlayerState playerState;
+        public bool isSuper;
+
+        public SuperZombie(Vector2 pos, Animation anim)
+        {
+            this.pos = pos;
+            this.animation = anim;
+
+
+            this.playerState = PlayerState.Run;
+            this.isSuper = true;
+        }
+
+
+        public void update(GameTime gameTime)
+        {
+            if (isSuper)
+            {
+                playerUpdate(gameTime);
+            }
+            else
+            {
+                zombieUpdate(gameTime);
+            }
+        }
+
+        public void render(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            this.animation.render(gameTime, spriteBatch, this.pos);
+
+        }
+
+        public void playerUpdate(GameTime gameTime)
+        {
+            switch (playerState)
+            {
+                case PlayerState.Run:
+                    if (G.input.Keyboard[Keys.A].IsDown)
+                    {
+                        vel.X = -1f;
+                    }
+                    else if (G.input.Keyboard[Keys.D].IsDown)
+                    {
+                        vel.X = 1f;
+                    }
+
+                    if (G.input.Keyboard[Keys.Space].IsPressed)
+                    {
+                        vel.Y = -3f;
+                        this.playerState = PlayerState.Jump;
+                        actionStart = gameTime;
+                    }
+
+                    if ((G.input.Keyboard[Keys.L].IsPressed))
+                    {
+                        this.playerState = PlayerState.Lunge;
+                        actionStart = gameTime;
+                    }
+
+                    this.pos += vel;
+                    //do collision detection here.
+
+
+                    break;
+
+                case PlayerState.Jump:
+                    //gravity accel
+                    vel.Y += 0.05f;
+
+                    //decide here if we want air control and how much
+                    if (G.input.Keyboard[Keys.A].IsDown)
+                    {
+                        vel.X = -1f;
+                    }
+                    else if (G.input.Keyboard[Keys.D].IsDown)
+                    {
+                        vel.X = 1f;
+                    }
+
+
+                    //update position
+                    this.pos += vel;
+
+                    //do collision detection here
+
+
+                    //temp
+                    if (this.pos.Y < 32 * 70)
+                    {
+                        this.playerState = PlayerState.Run;
+                    }
+
+
+                    break;
+
+                case PlayerState.Lunge:
+
+                    break;
+
+                case PlayerState.LungeStunned:
+
+                    break;
+
+                case PlayerState.Sit:
+
+                    break;
+
+                default:
+                    Console.Out.WriteLine("Player in invalid state");
+                    break;
+            }
+
+        }
+
+        public void zombieUpdate(GameTime gameTime)
+        {
+
+        }
+
     }
 }
