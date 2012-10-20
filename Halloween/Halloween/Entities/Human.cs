e76169db183d9ee58fa1e557973e6ffec5e0bf6d
Eric Dand <eric.c.dand@gmail.com>
2012-10-20 14:32:54 -0700

    Added typing for human pawns

--- Halloween/Halloween/Entities/Human.cs
+++ Halloween/Halloween/Entities/Human.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 
+
 namespace Halloween.Entities
 {
+    enum Type { kid, military, police }
     class Human : Pawn
     {
-        public bool isArmed;
-        
-        public Human() {
+        public Type type;
+        public bool armed;
+
+        public Human(Type type) {
             health = 2;
-            friendly = false;
-            isArmed = false;
+            this.type = type;
+            if (this.type != 0) {
+                armed = true;
+            }
         }
 
         public override void update(GameTime gameTime) {
-            if (isArmed) {
+            if (armed) {
                 //TODO: attack AI
             } else {
                 //TODO: run away AI
